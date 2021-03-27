ae8a0edd24e0a5649be74dda37db05e4564a1de4
rodrigues-heric <hericleiterodrigues@gmail.com>
2021-03-27 13:30:58 -0300

    Added simple window to show the result

--- dice-simulation.py
+++ dice-simulation.py
@@ -1,13 +1,33 @@
 import random
+import tkinter
 
 # Generate and return the dice number
 # null -> int
 def roll_dice():
     return random.randint(1, 6)
 
+# Make the window text
+# tkinter object, int, int -> void
+def make_text(window, altura, largura, msg):
+    t = tkinter.Text(window, height=altura, width=largura)
+    t.pack()
+    t.insert(tkinter.END, msg)
+
+# Create and configure the window
+# String, String -> tkinter object
+def create_window(title, msg):
+    window = tkinter.Tk()
+    window.title(title)
+    make_text(window, 5, 52, msg)
+    return window
+
 def main():
-    dice = roll_dice()
-    print(f'Dice number: {dice}')
+    # Window variables
+    title = "Dice Simulation"
+    msg = f'Dice number: {roll_dice()}'
+
+    main_window = create_window(title, msg)
+    main_window.mainloop()
 
 if __name__ == '__main__':
         main()
