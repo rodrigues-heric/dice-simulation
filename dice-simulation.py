c9ca07e44c49b2a776928e6dd00896475a16f692
rodrigues-heric <hericleiterodrigues@gmail.com>
2021-03-27 20:39:30 -0300

    now shows a tiny dice

--- dice-simulation.py
+++ dice-simulation.py
@@ -14,16 +14,31 @@ def roll_again(window, label):
         widget.destroy()
 
     # Recreate the widgets
-    make_text(window, 5, 52, f'Dice number: {roll_dice()}')
+    dice = roll_dice()
+    make_text(window, f'Dice number: {dice}')
+    make_img(window, dice)
     make_button(window, label)
 
+# Return the dice unicode
+# int -> String
+def dice_face(number):
+    faces = {
+        1: u'\u2680',
+        2: u'\u2681',
+        3: u'\u2682',
+        4: u'\u2683',
+        5: u'\u2684',
+        6: u'\u2685'
+    }
+    return faces[number]
+
 # Make the window text
-# tkinter object, int, int, String -> void
-def make_text(window, altura, largura, msg):
-    t = tkinter.Text(window, height=altura, width=largura)
-    t.pack()
-    t.insert(tkinter.END, msg)
-    t.config(state='disabled')
+# tkinter object, String -> void
+def make_text(window, msg):
+    t = tkinter.Label(window, 
+                        justify=tkinter.CENTER, 
+                        pady=20,
+                        text=msg).pack()
 
 # Make the button to roll the dice again
 def make_button(window, label):
@@ -31,22 +46,30 @@ def make_button(window, label):
                         command=lambda: roll_again(window, label))
     b.pack()
 
+# Insert the dice "image"
+# tkinter object, Int -> void
+def make_img(window, dice):
+    make_text(window, dice_face(dice))
+
 # Create and configure the window
-# String, String, String -> tkinter object
-def create_window(title, msg, btn_label):
+# String, Int, String, String -> tkinter object
+def create_window(title, dice_num, msg, btn_label):
     window = tkinter.Tk()
+    window.geometry('300x300')
     window.title(title)
-    make_text(window, 5, 52, msg)
+    make_text(window, msg)
+    make_img(window, dice_num)
     make_button(window, btn_label)
     return window
 
 def main():
     # Window variables
     title = 'Dice Simulation'
-    msg = f'Dice number: {roll_dice()}'
     btn_label = 'Roll dice'
+    dice_num = roll_dice()
+    msg = f'Dice number: {dice_num}'
 
-    main_window = create_window(title, msg, btn_label)
+    main_window = create_window(title, dice_num, msg, btn_label)
     main_window.mainloop()
 
 if __name__ == '__main__':
